5b64cf97d7f7b5607eb1e674f7eb7a041731c55a
Fabio Veroli <fabio.veroli@studio.unibo.it>
2024-01-31 18:25:00 +0100

    Create project using Maven

---
+++ smart-bridge/src/main/java/com/thesis/Main.java
@@ -0,0 +1,9 @@
+package com.thesis;
+
+import it.wldt.core.engine.WldtEngine;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
