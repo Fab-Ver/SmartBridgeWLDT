5a65521ac10bc9ca0f5c3812acb198a06a670596
Fabio Veroli <fabio.veroli@studio.unibo.it>
2024-02-05 13:42:54 +0100

    add method to configure sse properties

--- water-level-subsystem/src/main/java/com/thesis/digital/htpp/HttpDigitalAdapterConfiguration.java
+++ water-level-subsystem/src/main/java/com/thesis/digital/htpp/HttpDigitalAdapterConfiguration.java
@@ -1,6 +1,7 @@
 package com.thesis.digital.htpp;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.thesis.digital.htpp.exception.HttpDigitalAdapterConfigurationException;
@@ -12,30 +13,41 @@ public class HttpDigitalAdapterConfiguration {
     private final int port;
 
     private final Map<String, String> actionRoutes = new HashMap<>();
+    private final Map<String, List<String>> propertiesSseRoutes = new HashMap<>();
+    
     
     protected HttpDigitalAdapterConfiguration(int port, String host) {
         this.host = host;
         this.port = port;
     }
-
+    
     public static HttpDigitalAdapterConfigurationBuilder builder(int port, String host) throws HttpDigitalAdapterConfigurationException{
         return new HttpDigitalAdapterConfigurationBuilder(port, host);
     }
-
+    
     public String getHost() {
         return host;
     }
-
+    
     public Integer getPort() {
         return port;
     }
-
+    
     protected void addActionRoute(String actionKey, String actionRoute){
         this.actionRoutes.put(actionKey, actionRoute);
     }
-
+    
     public Map<String,String> getActionRoutes() {
         return actionRoutes;
     }
+    
+    public Map<String, List<String>> getPropertiesSse() {
+        return propertiesSseRoutes;
+    }
+
+    protected void addPropertySseRoute(List<String> propertiesKeys, String sseRoute){
+        propertiesSseRoutes.put(sseRoute, propertiesKeys);
+    }
+    
 
 }
--- water-level-subsystem/src/main/java/com/thesis/digital/htpp/HttpDigitalAdapterConfigurationBuilder.java
+++ water-level-subsystem/src/main/java/com/thesis/digital/htpp/HttpDigitalAdapterConfigurationBuilder.java
@@ -1,5 +1,7 @@
 package com.thesis.digital.htpp;
 
+import java.util.List;
+
 import com.thesis.digital.htpp.exception.HttpDigitalAdapterConfigurationException;
 
 public class HttpDigitalAdapterConfigurationBuilder {
@@ -18,22 +20,38 @@ public class HttpDigitalAdapterConfigurationBuilder {
         return this;
     }
 
+    public HttpDigitalAdapterConfigurationBuilder addPropertySSERoute(List<String> propertiesKeys, String sseRoute) throws HttpDigitalAdapterConfigurationException{
+        checkPropertyRoute(propertiesKeys, sseRoute);
+        this.configuration.addPropertySseRoute(propertiesKeys, sseRoute);
+        return this;
+    }
+
+    
     public HttpDigitalAdapterConfiguration build() {
         return this.configuration;
     }
-
+    
     private boolean isValid(String param){
         return param != null && !param.isEmpty();
     }
-
+    
     private boolean isValid(int param){
         return param <= 0;
     }
-
+    
     private void checkActionRoute(String actionKey, String actionRoute) throws HttpDigitalAdapterConfigurationException{
         if(!isValid(actionKey) || !isValid(actionRoute))
             throw new HttpDigitalAdapterConfigurationException("Action Key or Action Route cannot be empty or null");
         if(configuration.getActionRoutes().containsKey(actionKey))
             throw new HttpDigitalAdapterConfigurationException("Action Key already registered");
+    }
+
+    private void checkPropertyRoute(List<String> propertiesKeys, String sseRoute) throws HttpDigitalAdapterConfigurationException {
+        if(!isValid(sseRoute))
+            throw new HttpDigitalAdapterConfigurationException("SSE Route cannot be empty or null");
+        if(configuration.getPropertiesSse().containsKey(sseRoute))
+            throw new HttpDigitalAdapterConfigurationException("SSE Route already registered");
+        if(propertiesKeys.isEmpty() || propertiesKeys == null)
+            throw new HttpDigitalAdapterConfigurationException("Properties Keys List cannot be empty or null");    
     }
 }
